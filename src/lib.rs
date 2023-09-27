64fa34ffd9de34ef5e448727aeec3cfa07d16413
Alan Smith <commit@alanwsmith.com>
2023-09-27 15:23:50 -0400

    - starting to move to vec(Token, Range)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod chumsky;
 pub mod completion;
 pub mod jump_definition;
+pub mod neo_parser;
 pub mod reference;
 pub mod semantic_token;
---
+++ src/neo_parser.rs
@@ -0,0 +1,42 @@
+use chumsky::prelude::*;
+use chumsky::Parser;
+use std::ops::Range;
+
+#[derive(Debug)]
+pub enum Token {
+    Text(String),
+}
+
+fn non_less_than_char() -> impl Parser<char, Vec<(Token, Range<usize>)>, Error = Simple<char>> {
+    none_of("< \n\t")
+        .repeated()
+        .exactly(1)
+        .collect::<String>()
+        .map_with_span(|val, span| (Token::Text(val), span))
+        .repeated()
+        .at_least(1)
+}
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    pub fn basic() {
+        let left = "s";
+        let right = "s";
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn non_less_than_char_test_letter() {
+        let src = "a";
+        let left = Some(vec!['a']);
+        let (right, _err) = non_less_than_char().parse_recovery(src);
+        // assert_eq!(left, right);
+    }
+
+    //
+}
