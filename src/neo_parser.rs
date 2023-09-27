98ff8ca8f2ec034fa7ba4f2d672b651071303fe6
Alan Smith <commit@alanwsmith.com>
2023-09-27 15:44:00 -0400

    - getting initial character with leading < but not two <<

--- src/neo_parser.rs
+++ src/neo_parser.rs
@@ -7,14 +7,21 @@ pub enum Token {
     Text(String),
 }
 
-fn non_less_than_char() -> impl Parser<char, Vec<(Token, Range<usize>)>, Error = Simple<char>> {
+pub fn non_less_than_char() -> impl Parser<char, Vec<(Token, Range<usize>)>, Error = Simple<char>> {
     none_of("< \n\t")
         .repeated()
         .exactly(1)
         .collect::<String>()
         .map_with_span(|val, span| (Token::Text(val), span))
         .repeated()
-        .at_least(1)
+        .exactly(1)
+}
+
+pub fn less_than_with_char() -> impl Parser<char, Vec<(Token, Range<usize>)>, Error = Simple<char>>
+{
+    just('<')
+        .map_with_span(|val, span| (Token::Text(val.to_string()), span))
+        .chain(non_less_than_char())
 }
 
 #[cfg(test)]
@@ -31,10 +38,29 @@ mod test {
     }
 
     #[test]
-    fn non_less_than_char_test_letter() {
+    fn non_less_than_char_xxx_test_letter() {
         let src = "a";
         let left = Some(vec![(Token::Text("a".to_string()), 0..1)]);
         let (right, _err) = non_less_than_char().parse_recovery(src);
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn non_less_than_char_xxx_test_skip_lt_char() {
+        let src = "<";
+        let left = None;
+        let (right, _err) = non_less_than_char().parse_recovery(src);
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn less_than_with_char_xxx_basic_test() {
+        let src = "<b";
+        let left = Some(vec![
+            (Token::Text("<".to_string()), 0..1),
+            (Token::Text("b".to_string()), 1..2),
+        ]);
+        let (right, _err) = less_than_with_char().parse_recovery(src);
         assert_eq!(left, right);
     }
 
